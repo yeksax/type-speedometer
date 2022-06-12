c3f45f31613dbe59997b5b3e43c505fd2a71c94a
Hugo Almeida <hugoalmeida2412@gmail.com>
2022-06-12 16:27:45 -0300

    Refactored JS and fixed some bugs

--- script.js
+++ script.js
@@ -1,202 +1,137 @@
-const lorem = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni ad voluptates aperiam suscipit eveniet consequatur in eos earum assumenda dolor excepturi omnis officia facilis ullam repudiandae, cum modi esse quas.`
+const htmlText = $("#text")
+const htmlPointer = $("#pointer")
 
-let input = []
-let objectiveText = []
-let actualText = []
-let pointer = 0
-let lastKeyPress = 0
-let currentLine;
+let words = [
+    "pai", "m\u00e3e", "filho", "cachorro", "irm\u00e3o", "irm\u00e3", "empresa", "fam\u00edlia", "pa\u00eds", "pais", "casa", "chute", "bola", "carro", "preto", "branco", "amarelo", "vermelho", "azul", "cinza", "verde", "santos", "verdade", "falso", "quantidade", "em", "n\u00e3o", "sim", "poder", "minist\u00e9rio", "mist\u00e9rio", "camisa", "felicidade", "comida", "prazer", "comest\u00edvel", "vandalo", "fantasma", "porco", "colete", "jogar", "tudo", "esperar", "todo", "nada", "nem", "haver", "grande", "feliz", "sublime", "oni", "nojo", "triste", "tristeza", "parecer", "ficar", "fazer", "comer", "fora", "dj", "arana", "pop", "smoke", "pequeno", "grande", "pin\u00f3quio", "deserto", "chap\u00e9u", "tubo", "teste", "valorant", "elsword", "minecraft", "comunidade", "cringe"
+]
 
-const objective = document.querySelector("#objective")
-const htmlPointer = document.querySelector("#pointer")
-const lineHeight = Math.round(objective.getBoundingClientRect().height / 3)
+let htmlObjective;
 
-const afkDefault = 2000;
+let lineHeight;
+let actualText;
+let objective;
+let input;
+let pointer;
 
-let words;
-fetch("./words.json").then(r => {
-    r.json().then((json) => {
-        words = json
-        init()
+init(50)
 
-    })
-})
+$(document).on('keydown', e => keyHandler(e))
 
-function init() {
-
-    input = []
-    objectiveText = []
+function init(wordQuantity) {
+    htmlObjective = []
     actualText = []
+    objective = []
+    input = []
     pointer = 0
-    lastKeyPress = 0
-    currentLine;
 
-    for (let i = 0; i < 100; i++) {
-        objectiveText.push(words[ parseInt(Math.random() * words.length - 1) ])
-    }
-
-    objective.innerHTML = objectiveText.join(" ")
-
-    for (let i = 0; i < objectiveText.length; i++) {
+    for (let i = 0; i < wordQuantity; i++) {
         input.push("")
         actualText.push("")
+        objective.push(words[ Math.floor(Math.random() * wordQuantity) ])
+        htmlObjective.push(`<span class="word">${objective[ i ]}</span>`)
     }
 
-    pointerPos()
-    objective.scroll(0, 0)
+    htmlText.html(htmlObjective.join(" "))
+    lineHeight = $(htmlText.children()[ 0 ]).height()
+    autoScroll()
 }
 
-function pointerPos() {
-    let lastWord = objective.children[ objective.children.length - 1 ]
-    let charPos;
-    let x, y;
+// Trata de todos inputs 
+function keyHandler(e) {
+    let isValidInput = /^.$/u.test(e.key)
 
-    if (!lastWord) {
-        charPos = objective.getBoundingClientRect()
-
-        x = charPos.left - 2
-        y = charPos.top + 1
-    } else {
-        let lastChar = lastWord.children[ lastWord.children.length - 1 ]
-
-        charPos = lastChar.getBoundingClientRect();
-        x = charPos.left + charPos.width - 2
-        y = charPos.top - 3
-    }
-
-    if (lastWord && input[ pointer ].length == 0) {
-        x += 16
-    }
-
-    htmlPointer.style.left = `${x}px`
-    htmlPointer.style.top = `${y}px`
-}
-
-function wordMatch(w1, w2) {
-    return w1 == w2
-}
-
-function charMatch(w1, w2) {
-    let res = "";
-    let w1l = w1.length
-    let w2l = w2.length
-    let match = true
-
-    if (w2l == 0) {
-        return w1
-    }
-
-
-    for (let i = 0; i < w1l; i++) {
-
-        if (w2[ i ] == undefined) {
-            res += w1[ i ]
-            continue
-        }
-
-        if (w1[ i ] == w2[ i ]) {
-            res += `<span class="correct">${w2[ i ]}</span>`
-        }
-
-        if (w1[ i ] != w2[ i ]) {
-            match = false
-            res += `<span class="incorrect">${w1[ i ]}</span>`
-        }
-
-    }
-
-    if (w2l > w1l) {
-        let toBeIgnored = w2.substring(0, w1l)
-        res += `<span class="overflow">${w2.replace(toBeIgnored, "")}</span>`
-    }
-
-    if (!match) {
-        return `<span class="incorrect-word">${res}</span>`
-    } else {
-        return `<span class="word">${res}</span>`
-    }
-}
-
-document.addEventListener("keydown", (e) => {
-    if (e.key == ' ') {
-        pointer++
+    if (isValidInput) {
+        inputHandler(e.key)
     } else if (e.key == 'Backspace') {
-        if (input[ pointer ].length > 0) {
+        let canDelete = input[ pointer ].length > 0
+        let canGoBack = input[ pointer - 1 ] && !(input[ pointer - 1 ] == objective[ pointer - 1 ])
+
+        if (canDelete) {
             input[ pointer ] = input[ pointer ].substring(0, input[ pointer ].length - 1)
-        } else if (pointer > 0 && !wordMatch(objectiveText[ pointer - 1 ], input[ pointer - 1 ])) {
+        } else if (canGoBack) {
             pointer--
         }
-    } else {
-        if (e.key.length == 1) {
-            input[ pointer ] += e.key
-            lastKeyPress = new Date().getTime()
-        }
     }
 
-    for (let i = 0; i < objectiveText.length; i++) {
-        if (!wordMatch(objectiveText[ i ], input[ i ]) && pointer != i) {
-            actualText[ i ] = `<span class="incorrect-word">${actualText[ i ]}</span>`
-
-        }
-        actualText[ i ] = charMatch(objectiveText[ i ], input[ i ])
-    }
-
-    objective.innerHTML = actualText.join(" ")
-
+    actualText[ pointer ] = charMatching(objective[ pointer ], input[ pointer ])
+    $(htmlText.children()[ pointer ]).html(actualText[ pointer ]);
     autoScroll()
-})
-
-function restart() {
-    for (let i = 0; i < 100; i++) {
-        objectiveText.push(words[ parseInt(Math.random() * words.length - 1) ])
-    }
-
-    objective.innerHTML = objectiveText.join(" ")
-
-    for (let i = 0; i < objectiveText.length; i++) {
-        input.push("")
-        actualText.push("")
-    }
-
-    document.querySelector("#restart").blur()
-
-    init()
-    pointerPos()
 }
 
-document.addEventListener('keydown', (e) => {
-    if (e.key == "Tab") {
-        e.preventDefault()
-        document.querySelector("#restart").focus()
+// Trata de inputs válidos
+function inputHandler(key) {
+    if (key == " ") {
+        if (input[ pointer ].length > 0) {
+            pointer++
+        }
+    } else if (input[ pointer ].length <= 16) {
+        input[ pointer ] += key
     }
-})
 
+}
 
-function afkCheck() {
-    if (new Date().getTime() - lastKeyPress >= afkDefault) {
-        if (!htmlPointer.classList.contains("afk")) htmlPointer.classList.add("afk")
+function charMatching(obj, inp) {
+    let text = ""
+
+    let isCorrect = obj == inp
+    let biggestWord = Math.max(obj.length, inp.length)
+
+    for (let i = 0; i < biggestWord; i++) {
+        let inp_char = inp[ i ]
+        let obj_char = obj[ i ]
+
+        if (!obj[ i ]) {
+            text += `<span class="overflow">${inp_char}</span>`
+            continue
+        }
+        if (!inp[ i ]) {
+            text += obj_char
+            continue
+        }
+        if (inp_char == obj_char) {
+            text += `<span class="correct">${obj_char}</span>`
+        }
+        if (inp_char != obj_char) {
+            text += `<span class="incorrect">${obj_char}</span>`
+        }
+
+    }
+
+    if (inp.length >= obj.length && !isCorrect) {
+        $(htmlText.children()[ pointer ]).addClass("incorrect-word")
+        return text
     } else {
-        if (htmlPointer.classList.contains("afk")) htmlPointer.classList.remove("afk")
+        $(htmlText.children()[ pointer ]).removeClass("incorrect-word")
+        return text
     }
 }
-
-setInterval(() => {
-    afkCheck()
-}, 1000);
 
 function autoScroll() {
-    let lastWord = objective.children[ objective.children.length - 1 ]
-    let lastChar = objective.children[ objective.children.length - 1 ]
+    let currentLine = Math.round(($(htmlText.children()[ pointer ]).offset().top - htmlText.offset().top) / lineHeight);
 
-    if (!lastWord) {
-        lastChar = 0
+    $("#objective").scrollTop(lineHeight * currentLine - lineHeight + 1)
+    pointerPosition()
+}
+
+function pointerPosition() {
+    let currentWord = $(htmlText.children()[ pointer ]);
+    let currentChar = $(currentWord.children()[ currentWord.children().length - 1 ]);
+
+    let x, y;
+
+    if (currentChar.length) {
+        x = currentChar.offset().left + currentChar.width()
+        y = currentChar.offset().top
     } else {
-        lastChar = lastWord.children[ lastWord.children.length - 1 ].offsetTop
+        x = currentWord.offset().left
+        y = currentWord.offset().top
     }
 
-    y = lastChar
+    y += 2
 
-    objective.scrollTo(0, y - (lineHeight))
+    htmlPointer.css("top", `${y}px`)
+    htmlPointer.css("left", `${x}px`)
 
-    pointerPos()
+    console.log(x, y);
+    console.log(htmlPointer.position());
 }
